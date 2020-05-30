744e43cd5560c472167de3e4b831cbc7285fa0b3
Conor <conor.heeney56@gmail.com>
2020-05-30 13:27:53 +0100

    Implemented cell blocking functionality

--- pathfinding.py
+++ pathfinding.py
@@ -1,4 +1,5 @@
 from apq import *
+import math
 import pygame as pg
 from pygame.locals import *
 from settings import *
@@ -34,95 +35,6 @@ class Grid:
                 if y < len(self.structure)-1 and x > 0:
                     self.structure[y][x].neighbours[(y+1, x-1)] = 2
 
-class GUI:
-
-    def __init__(self, num_rows, num_cols):
-        pg.init()
-        self.grid = Grid(num_rows, num_cols)
-        self.cell_size = CELL_SIZE
-        self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.start_ver = (15, 15)
-        self.target_ver = (20, 20)
-        
-    def run(self):
-        self.running = True
-        while self.running:
-            self.events()
-        pg.quit()
-
-    def draw(self):
-        self.screen.fill(BLACK)
-        for i in range(len(self.grid.structure)):
-            for j in range(len(self.grid.structure[i])):
-                if (j, i) in self.grid.locs:
-                    pg.draw.rect(self.screen, YELLOW, (j*self.cell_size+j, i*self.cell_size+i, self.cell_size, self.cell_size))
-                if (j, i) in self.grid.closed:
-                    pg.draw.rect(self.screen, RED, (j*self.cell_size+j, i*self.cell_size+i, self.cell_size, self.cell_size))
-                if (j, i) not in self.grid.closed and (j, i) not in self.grid.locs:
-                    pg.draw.rect(self.screen, WHITE, (j*self.cell_size+j, i*self.cell_size+i, self.cell_size, self.cell_size))
-        pg.display.flip()
-
-    def events(self):
-        for event in pg.event.get():
-            if event.type == QUIT:
-                self.running = False
-            elif event.type == KEYDOWN:
-                if event.key == K_SPACE:
-                    self.dijkstra((1, 2), (20, 20))
-
-    def trace_path(self, index):
-        if index == None:
-            return True
-        else:
-            index_x = index[1]
-            index_y = index[0]
-            pg.draw.rect(self.screen, BLUE, (index_y*self.cell_size+index_y, index_x*self.cell_size+index_x, self.cell_size, self.cell_size))
-            pg.display.flip()
-            pg.time.delay(100)
-            self.trace_path(self.grid.closed[index][1])
-
-    #Implementation of dijkstra using my own implementation of a min binary heap for the open cells
-    # Locs stores the references to the open cells in the priority queue so that the algorithm can check whether
-    # A cell is in open
-    def dijkstra(self, source, destination):
-
-        self.grid.open = APQ()
-        self.grid.closed = {}
-        self.grid.locs = {}
-        self.grid.preds = {source: None}
-
-        elt = self.grid.open.add(0, source)
-        self.grid.locs[source] = elt
-        self.draw()
-        while self.grid.open.length() != 0:
-            for event in pg.event.get():
-                if event.type == QUIT:
-                    self.running = False
-            min = self.grid.open.remove_min()
-            minElt = min.value
-            minCost = min.key
-            self.grid.locs.pop(minElt)
-            predecessor = self.grid.preds.pop(minElt)
-            self.grid.closed[minElt] = (minCost, predecessor)
-            self.draw()
-            if minElt == destination:
-                self.trace_path(self.target_ver)
-                return self.grid.closed
-            for cellLocation in self.grid.structure[minElt[0]][minElt[1]].neighbours:
-                neighbourCell = cellLocation
-                if neighbourCell not in self.grid.closed:
-                    newcost = minCost + self.grid.structure[minElt[0]][minElt[1]].neighbours[neighbourCell]
-                    if neighbourCell not in self.grid.locs:
-                        self.grid.preds[neighbourCell] = minElt
-                        elt = self.grid.open.add(newcost, neighbourCell)
-                        self.grid.locs[neighbourCell] = elt
-                        self.draw()
-                    elif newcost < self.grid.locs[neighbourCell].key:
-                        self.grid.preds[neighbourCell] = minElt
-                        self.grid.locs[neighbourCell].key = newcost
-                        self.draw()
-
-
     
 class Cell:
 
@@ -130,14 +42,122 @@ class Cell:
         self.x = x
         self.y = y
         self.neighbours = {}
+        self.in_open = False
+        self.in_closed = False
+        self.cell_size = CELL_SIZE
+        self.blocked = False
 
     def __str__(self):
         outstr = f"{self.x}, {self.y}"
         return outstr
 
+    def draw_cell(self):
+        if not self.blocked:
+            if self.in_open:
+                pg.draw.rect(screen, YELLOW, (self.x*self.cell_size+self.x, self.y*self.cell_size+self.y, self.cell_size, self.cell_size))
+            elif self.in_closed:
+                pg.draw.rect(screen, RED, (self.x*self.cell_size+self.x, self.y*self.cell_size+self.y, self.cell_size, self.cell_size))
+            else:
+                pg.draw.rect(screen, WHITE, (self.x*self.cell_size+self.x, self.y*self.cell_size+self.y, self.cell_size, self.cell_size))
+        else:
+            pg.draw.rect(screen, BLACK, (self.x*self.cell_size+self.x, self.y*self.cell_size+self.y, self.cell_size, self.cell_size))
+
+def run(source, dest):
+        running = True
+        draw()
+        while running:
+            for event in pg.event.get():
+                if event.type == QUIT:
+                    running = False
+                elif event.type == KEYDOWN:
+                    if event.key == K_SPACE:
+                        dijkstra(source, dest)
+                elif event.type == MOUSEBUTTONDOWN:
+                    position = pg.mouse.get_pos()
+                    block(position)
+        pg.quit()  
+
+def draw():
+    screen.fill(BLACK)
+    for i in range(len(grid.structure)):
+        for j in range(len(grid.structure[i])):
+            grid.structure[i][j].draw_cell()
+    pg.display.flip()
+
+
+def trace_path(index, closed):
+    if index == None:
+        return True
+    else:
+        index_x = index[1]
+        index_y = index[0]
+        cell_size = grid.structure[index_y][index_x].cell_size
+        pg.draw.rect(screen, BLUE, (index_x*cell_size+index_x, index_y*cell_size+index_y, cell_size, cell_size))
+        pg.display.flip()
+        pg.time.delay(100)
+        trace_path(closed[index][1], closed)
+
+def block(position):
+        x = position[0]
+        y = position[1]
+        row = math.ceil(y // CELL_SIZE) - 1
+        col = math.ceil(x // CELL_SIZE) - 1
+        cell = grid.structure[row][col]
+        cell.blocked = True
+        draw()
+
+
+#Implementation of dijkstra using my own implementation of a min binary heap for the open cells
+# Locs stores the references to the open cells in the priority queue so that the algorithm can check whether
+# A cell is in open
+def dijkstra(source, destination):
+
+    open = APQ()
+    closed = {}
+    locs = {}
+    preds = {source: None}
+
+    elt = open.add(0, source)
+    locs[source] = elt
+    grid.structure[source[0]][source[1]].in_open = True
+    draw()
+    while open.length() != 0:
+        for event in pg.event.get():
+            if event.type == QUIT:
+                running = False
+        min = open.remove_min()
+        minElt = min.value
+        minCost = min.key
+        min_cell = grid.structure[minElt[0]][minElt[1]]
+        locs.pop(minElt)
+        predecessor = preds.pop(minElt)
+        closed[minElt] = (minCost, predecessor)
+        min_cell.in_open = False
+        min_cell.in_closed = True
+        draw()
+        if minElt == destination:
+            trace_path(destination, closed)
+            return closed
+        for cellLocation in grid.structure[minElt[0]][minElt[1]].neighbours:
+                neighbour_cell_obj = grid.structure[cellLocation[0]][cellLocation[1]]
+                if not neighbour_cell_obj.in_closed and not neighbour_cell_obj.blocked:
+                    newcost = minCost + grid.structure[minElt[0]][minElt[1]].neighbours[cellLocation]
+                    if cellLocation not in locs:
+                        preds[cellLocation] = minElt
+                        elt = open.add(newcost, cellLocation)
+                        locs[cellLocation] = elt
+                        neighbour_cell_obj.in_open = True
+                        draw()
+                    elif newcost < locs[cellLocation].key:
+                        preds[neighbourCell] = minElt
+                        locs[neighbourCell].key = newcost
+                        draw()
+
+pg.init()
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+grid = Grid(ROWS, COLS)
 
 def main():
-    gui = GUI(30, 30)
-    gui.run()
+    run((2, 7), (20, 20))
 
 main()
